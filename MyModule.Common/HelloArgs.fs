63a1fe031b790a413c40adf8942e6df0aa8c18d4
Arshia001 <arshia001@live.com>
2019-07-25 15:24:14 +0430

    Add project files.

---
+++ MyModule.Common/HelloArgs.fs
@@ -0,0 +1,14 @@
+﻿module HelloArgs
+
+(* 
+   Any types that need to be in the public interface of the
+   module go in a separate DLL which I'll call "Common". This
+   is because we'll codegen the entire grain interface assembly,
+   so users can't really place any other code in there.
+*)
+
+type T = {
+    Name: string
+    }
+
+let create name = { Name = name }
